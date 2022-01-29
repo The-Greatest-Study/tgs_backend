a3146407062b00c5e08bbb379643db7845ae5534
minnczi <kmj1995kr@gmail.com>
2022-01-29 17:16:00 +0900

    ADD - 우리가 해냈다

---
+++ src/main/java/com/tgs/tgs_backend/TgsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tgs.tgs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgs/tgs_backend/TgsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgs.tgs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
